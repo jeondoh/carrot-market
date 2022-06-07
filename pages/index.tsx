78263804057b99197b523da7aaaf766301fe1e2c
jundoh <jundo1302@gmail.com>
2022-06-07 23:20:50 +0900

    feat: tailwind modifiers

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,8 +19,10 @@ const Home: NextPage = () => {
             <span>Total</span>
             <span className="font-semibold">$10</span>
           </div>
-          <div className="mt-5 bg-blue-500 text-white p-3 text-center rounded-xl w-3/4 mx-auto">
-            Checkout
+          <div className="text-center">
+            <button className="mt-5 bg-blue-500 text-white p-3  rounded-xl w-3/4 hover:bg-teal-500 hover:text-black active:bg-yellow-500 focus:bg-red-500">
+              Checkout
+            </button>
           </div>
         </div>
         {/* 2 */}
@@ -61,9 +63,9 @@ const Home: NextPage = () => {
             <span className="text-xs text-gray-500">Chair</span>
             <div className="mt-3 mb-5 flex justify-between items-center">
               <div className="space-x-2">
-                <input type="radio" />
-                <input type="radio" />
-                <input type="radio" />
+                <button className="w-5 h-5 rounded-full bg-yellow-500 focus:ring-2 ring-offset-2 ring-yellow-500 transition-colors" />
+                <button className="w-5 h-5 rounded-full bg-indigo-500 focus:ring-2 ring-offset-2 ring-indigo-500 transition-colors" />
+                <button className="w-5 h-5 rounded-full bg-teal-500 focus:ring-2 ring-offset-2 ring-teal-500 transition-colors" />
               </div>
               <div className="flex items-center space-x-5">
                 <button className="rounded-lg bg-blue-200 flex justify-center items-center aspect-square w-8 text-xl text-gray-500">
