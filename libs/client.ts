e2b3a2824b94262044b02afe6d912feb99d7f7c0
jundoh <jundo1302@gmail.com>
2022-06-16 23:50:14 +0900

    feat: make api route to use prisma

--- libs/client.ts
+++ libs/client.ts
@@ -1,9 +1,3 @@
 import { PrismaClient } from "@prisma/client";
 
-const client = new PrismaClient();
-client.user.create({
-  data: {
-    name: "jeondoh",
-    email: "jundo1302@gmail.com",
-  },
-});
+export default new PrismaClient();
---
+++ pages/api/client-test.tsx
@@ -0,0 +1,18 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import client from "../../libs/client";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  await client.user.create({
+    data: {
+      name: "jeondoh",
+      email: "jundo1302@gmail.com",
+    },
+  });
+
+  res.json({
+    ok: true,
+  });
+}
