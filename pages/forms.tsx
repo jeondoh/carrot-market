3426c338340cae8f0e095c5b8716d07cba533406
jundoh <jundo1302@gmail.com>
2022-06-19 23:24:01 +0900

    fix: use react-hook-form

--- pages/forms.tsx
+++ pages/forms.tsx
@@ -1,61 +1,13 @@
-import React, { useState } from "react";
+import { useForm } from "react-hook-form";
 
 export default function Forms() {
-  const [username, setUsername] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [formErrors, setFormErrors] = useState("");
-  const [emailError, setEmailError] = useState("");
-  const onUsernameChange = (event: React.SyntheticEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = event;
-    setUsername(value);
-  };
-  const onEmailChange = (event: React.SyntheticEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = event;
-    setEmail(value);
-    setEmailError("");
-  };
-  const onPasswordChange = (event: React.SyntheticEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = event;
-    setPassword(value);
-  };
-  const onSubmit = (event: React.SyntheticEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    if (username === "" || email === "" || password === "") {
-      setFormErrors("All fields are required");
-    }
-    if (!email.includes("@")) {
-      setEmailError("email is required");
-    }
-  };
+  const { register, watch } = useForm();
 
   return (
-    <form onSubmit={onSubmit}>
-      <input
-        value={username}
-        onChange={onUsernameChange}
-        type="text"
-        placeholder="Username"
-      />
-      <input
-        value={email}
-        type="email"
-        onChange={onEmailChange}
-        placeholder="Email"
-      />
-      {emailError}
-      <input
-        value={password}
-        type="password"
-        onChange={onPasswordChange}
-        placeholder="Password"
-      />
+    <form>
+      <input {...register("username")} type="text" placeholder="Username" />
+      <input {...register("email")} type="email" placeholder="Email" />
+      <input {...register("password")} type="password" placeholder="Password" />
       <input type="submit" value="Create Account" />
     </form>
   );
