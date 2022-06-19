b69bd716506783271b3943dc9a8605ecb4b069d9
jundoh <jundo1302@gmail.com>
2022-06-19 23:41:15 +0900

    fix: validation to use react-hook-form

--- pages/forms.tsx
+++ pages/forms.tsx
@@ -1,13 +1,43 @@
-import { useForm } from "react-hook-form";
+import { FieldErrors, useForm } from "react-hook-form";
+
+interface ILoginForm {
+  username: string;
+  password: string;
+  email: string;
+}
 
 export default function Forms() {
-  const { register, watch } = useForm();
+  const { register, watch, handleSubmit } = useForm<ILoginForm>();
+  const onValid = (data: ILoginForm) => {
+    console.log("im valid bby");
+  };
+  const onInvalid = (errors: FieldErrors) => {
+    console.log(errors);
+  };
 
   return (
-    <form>
-      <input {...register("username")} type="text" placeholder="Username" />
-      <input {...register("email")} type="email" placeholder="Email" />
-      <input {...register("password")} type="password" placeholder="Password" />
+    <form onSubmit={handleSubmit(onValid, onInvalid)}>
+      <input
+        {...register("username", {
+          required: "username is required",
+          minLength: {
+            message: "The username should be longer than 5 chars",
+            value: 5,
+          },
+        })}
+        type="text"
+        placeholder="Username"
+      />
+      <input
+        {...register("email", { required: "email is required" })}
+        type="email"
+        placeholder="Email"
+      />
+      <input
+        {...register("password", { required: "password is required" })}
+        type="password"
+        placeholder="Password"
+      />
       <input type="submit" value="Create Account" />
     </form>
   );
