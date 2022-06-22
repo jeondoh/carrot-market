6f30f8157e19fe293666a60434316c1954a6fec1
jundoh <jundo1302@gmail.com>
2022-06-22 23:42:33 +0900

    feat: Accounts Logic with prisma

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -3,7 +3,20 @@ import withHandler from "@libs/server/withHandler";
 import client from "@libs/client/client";
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
-  console.log(req.body);
+  const { phone, email } = req.body;
+  const payload = phone ? { phone: +phone } : { email };
+  // phone or email을 key로 설정, create / update
+  const user = await client.user.upsert({
+    where: {
+      ...payload,
+    },
+    create: {
+      name: "Anonymous",
+      ...payload,
+    },
+    update: {},
+  });
+
   res.status(200).end();
 }
 
