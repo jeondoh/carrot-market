cfe39f9c736140d6289b3a5177a7c945c806d2c3
jundoh <jundo1302@gmail.com>
2022-06-24 23:49:58 +0900

    fix: refectory code

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,32 +1,36 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import withHandler from "@libs/server/withHandler";
+import withHandler, { IResponseType } from "@libs/server/withHandler";
 import client from "@libs/client/client";
 
-async function handler(req: NextApiRequest, res: NextApiResponse) {
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<IResponseType>
+) {
   const { phone, email } = req.body;
-  const payload = phone ? { phone: +phone } : { email };
-
+  const user = phone ? { phone: +phone } : email ? { email } : null;
+  if (!user) return res.status(400).json({ ok: false });
+  const payload = Math.floor(100000 + Math.random() * 900000) + "";
   const token = await client.token.create({
     data: {
-      payload: "1234",
+      payload,
       user: {
         connectOrCreate: {
           // connectOrCreate : 유저를 찾지 못했을 때 생성해줌
           // user 테이블의 phone or email을 key로 설정, create
           where: {
-            ...payload,
+            ...user,
           },
           create: {
             name: "Anonymous",
-            ...payload,
+            ...user,
           },
         },
       },
     },
   });
-  console.log(token);
-
-  res.status(200).end();
+  return res.json({
+    ok: true,
+  });
 }
 
 export default withHandler("POST", handler);
