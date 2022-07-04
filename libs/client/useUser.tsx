4900e70ee513570e6dd15781cea1c2d11bbf2a07
jundoh <jundo1302@gmail.com>
2022-07-04 23:51:41 +0900

    refact: refactoring code useUser to use swr

--- libs/client/useUser.tsx
+++ libs/client/useUser.tsx
@@ -2,13 +2,15 @@ import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import useSWR from "swr";
 
-const fetcher = (url: string) => fetch(url).then((res) => res.json());
-
 export default function useUser() {
-  const { data, error } = useSWR("/api/users/me", fetcher);
+  const { data, error } = useSWR("/api/users/me");
   const router = useRouter();
 
-  // return router.replace("/enter");
+  useEffect(() => {
+    if (data && !data.ok) {
+      router.replace("/enter");
+    }
+  }, [data, router]);
 
-  return data;
+  return { user: data?.profile, isLoading: !data && !error };
 }
